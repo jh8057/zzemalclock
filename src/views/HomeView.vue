4150edd39305e6b00f0b9b6b15084e2d27ddf0fe
jh8057 <luckysymbol13@gmail.com>
2023-03-21 22:16:04 +0900

    [clock] button
    - link to github button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,14 @@ import MainClock from "../components/MainClock.vue";
 
 <template>
   <main>
-    <h1>main clock</h1>
+    <h1>Clock</h1>
     <main-clock />
+    <a
+      class="main__link"
+      href="https://github.com/jh8057/zzemalclock"
+      target="_blank"
+      >소스 코드 보기(github)</a
+    >
   </main>
 </template>
 
@@ -17,5 +23,23 @@ main {
   flex-direction: column;
   justify-content: center;
   text-align: center;
+}
+
+.main__link {
+  width: 100px;
+  color: white;
+  text-decoration: none;
+  text-align: center;
+  justify-content: center;
+  margin: auto;
+  margin-top: 40px;
+  border-radius: 7px;
+  transition: transform 0.3s;
+  padding: 0.75em 1.75em;
+  background-image: linear-gradient(135deg, var(--darkBlue), var(--lightBlue));
+}
+
+.main__link:hover {
+  transform: translateX(20px);
 }
 </style>
